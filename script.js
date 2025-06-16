82363d8471981620407a3a26fdab3d0f06fc13fc
ibrohm <ibrohimfarruxov666@gmail.com>
2025-06-16 15:34:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const ism = prompt("Ismingizni kiriting:");
+const familiya = prompt("Familiyangizni kiriting:");
+const yosh = prompt("Yoshingizni kiriting:");
+const fan = prompt("yoqtirgan faningizni yozing:");
+const tuman = prompt("yashaydigan tumaningizni kiriting:");
+alert("Ma'lumotlaringiz uchun rahmat.");
